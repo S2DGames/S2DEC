28c559798f34596bb02a6e399e0d16241185c4c8
domadden <dmadden772@gmail.com>
2015-04-18 15:01:02 -0400

    main

--- S2DEC/main.cpp
+++ S2DEC/main.cpp
@@ -1,30 +1,31 @@
 #include "Game.h"
-#include "ShipImage.h"
-#include "StarImage.h"
-#include "Gun.h"
-#include <random>
+#include "Computer Hacker/HackerPhysics.h"
+#include "Computer Hacker/HackerImage.h"
+#include "Computer Hacker/Floor.h"
+#include "Computer Hacker/EnemyMovement.h"
+#include "Computer Hacker/EnemyPhysics.h"
+#include "Computer Hacker/EnemyImage.h"
+#include "Util.h"
 
 using namespace S2D;
-using std::default_random_engine;
-using std::uniform_real_distribution;
 
 int main(){
 	Game game(1280, 720, "S2DEC");
-	default_random_engine rng;
-	uniform_real_distribution<float> xDist(-(float)Game::SCREEN_SIZE.x, (float)Game::SCREEN_SIZE.x);
-	uniform_real_distribution<float> yDist(-(float)Game::SCREEN_SIZE.y, (float)Game::SCREEN_SIZE.y);
-	uniform_real_distribution<float> offsetDist(0.0f, 30.65f);
+	game.SetGravity({0, .5 * SCALE});
 	
-	for(int i = 0; i < 1000; i++){
-		Entity& star = game.createEntity("star" + std::to_string(i));
-		star.addComponent<StarImage>(&game, "resources/stars.png", sf::Vector2f{xDist(rng), yDist(rng)}, offsetDist(rng));
-	}
+	Entity& hacker = game.createEntity("Hacker");
+	hacker.addComponent<HackerPhysics>(&game);
+	hacker.addComponent<HackerImage>(&game, "resources/hacker.png");
 
-	Entity& ship = game.createEntity("ship");
-	ship.addComponent<ShipControls>(&game);
-	ship.addComponent<ShipPhysics>(&game);
-	ship.addComponent<ShipImage>("resources/ship.png");
-	ship.addComponent<Gun>(&game);
+	Entity& floor = game.createEntity("Floor");
+	Floor& f = floor.addComponent<Floor>(&game);
+	f.addVertex({0.0f / SCALE, 720.0f / SCALE});
+	f.addVertex({1280.0f / SCALE, 720.0f / SCALE});
+
+	Entity& enemy = game.createEntity("Enemy");
+	enemy.addComponent<EnemyMovement>(&game, patrol, sf::Vector2f{600, 500}, sf::Vector2f{700, 500});
+	enemy.addComponent<EnemyPhysics>(&game);
+	enemy.addComponent<EnemyImage>("resources/hacker.png");
 
 	game.init();
 	game.play();
